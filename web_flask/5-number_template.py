16a33f3b9d94bd09165d480f1ddd6584c75f51df
SebastianBlandon <blandonodz@gmail.com>
2022-09-14 00:47:44 -0500

    [UPDATED] 5-number_template.py file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    Number Flask!
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', num=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
