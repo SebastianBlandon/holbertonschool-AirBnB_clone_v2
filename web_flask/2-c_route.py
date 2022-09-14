863686d4e206c8f2e9de58e776c625f4d8d5e5a7
SebastianBlandon <blandonodz@gmail.com>
2022-09-14 00:36:35 -0500

    [UPDATED] 2-c_route.py file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    HBNB Flask!
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
